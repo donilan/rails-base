0186c9ce6f8309373d1162ff17da37c95d16c62b
Doni Leung <doni@kudelabs.com>
2017-02-22 15:43:57 +0800

    add helper to show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def paginate objects, options = {}
+    options.reverse_merge!( theme: 'twitter-bootstrap-3' )
+    super( objects, options )
+  end
+
+  def alert_class(type)
+    case type
+    when "error", 'alert'
+      return "alert-danger"
+    when 'success'
+      return "alert-success"
+    when 'info', "notice"
+      return 'alert-info'
+    when 'warning'
+      return "alert-warning"
+    else
+      Rails.logger.error "flash type '#{type}' not avaliable, please use one of 'error', 'success', 'info' or 'notice'"
+    end
+  end
 end
