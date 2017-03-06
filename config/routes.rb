8f7d7d869898e9d918b9f374361784c7e6175e61
Doni Leung <doni@kudelabs.com>
2017-03-06 18:02:20 +0800

    add devise view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+               confirmations: 'users/confirmations',
+               passwords: 'users/passwords',
+               registrations: 'users/registrations',
+               unlocks: 'users/unlocks',
+               omniauth: 'users/omniauth'
+             }, skip: [:sessions]
+  as :user do
+    get 'signin', to: 'users/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    get 'signout', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
   root 'home#index'
 
   namespace :admin do
